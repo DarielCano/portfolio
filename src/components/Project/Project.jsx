422d04dfcbe0da8697b62872c2b06c1a9c8c8465
Dariel Cano <dariel.cano1992@gmail.com>
2023-06-12 10:46:47 -0600

    cambios al componente project

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -2,12 +2,7 @@ import "./Project.css";
 import "animate.css";
 const Project = ({ project }) => {
   return (
-    <a
-      href={project?.link}
-      target="_blank"
-      rel="noreferrer"
-      className="project-container animate__animated animate__zoomIn"
-    >
+    <div className="project-container animate__animated animate__zoomIn">
       <div className="project-container__img">
         <img src={project?.src} alt={project.title} />
       </div>
@@ -15,8 +10,17 @@ const Project = ({ project }) => {
       <div className="project-container__text">
         <h2>{project?.title}</h2>
         <p>{project?.description}</p>
+
+        <a
+          href={project?.link}
+          className="btn btn-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Ir al Sitio
+        </a>
       </div>
-    </a>
+    </div>
   );
 };
 
